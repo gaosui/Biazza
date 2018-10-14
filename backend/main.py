ee61d26da5eedfc3f29dfe70b81f2ad84a79fbd8
Sui Gao <gaosui2051@gmail.com>
2018-10-14 00:42:23 -0700

    backend

--- backend/main.py
+++ backend/main.py
@@ -1,24 +1,37 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 import sys
+import urllib.parse as up
 
 
 f = open('cache.json')
 cache = json.load(f)
 f.close()
+classCode = None
 
 
-print(cache['jml6wogpji0o3'][sys.argv[1]]['content'])
-
-#from http.server import BaseHTTPRequestHandler, HTTPServer
-
-
-# class Handler(BaseHTTPRequestHandler):
-#     def do_GET(self):
-#         print(self.path.split('/')[-1])
-#         self.send_response(200)
-#         self.send_header('Access-Control-Allow-Origin', '*')
-#         self.end_headers()
-#         return
+def search(key, dict):
+    pass
 
 
-# HTTPServer(('', 3000), Handler).serve_forever()
+def buildDict(cid):
+    res = {}
+    for k, v in cache[cid].items():
+        res[k] = v['content']
+    return res
+
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        args = up.parse_qs(self.path.split('?')[1])
+
+        dic = buildDict(args['cid'][0])
+        search(args['key'][0], dic)
+
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+        return
+
+
+HTTPServer(('', 3000), Handler).serve_forever()
---
+++ extension/contentScript.js
@@ -0,0 +1,44 @@
+console.log('hohohohohohohhoohoo')
+let root = document.createElement('div')
+root.classList.add('biazza')
+document.querySelector('#views').appendChild(root)
+
+let bar = document.createElement('div')
+bar.classList.add('dashboard_toolbar', 'biazza_bar')
+bar.textContent = 'Biazza'
+root.appendChild(bar)
+
+
+
+let page_center = document.querySelector('#page_center')
+let keywordQueue = []
+document.querySelector('#search-box').addEventListener('input', e => {
+  if (e.target.value) {
+    keywordQueue.push(e.target.value)
+    root.classList.add('active')
+    page_center.classList.add('active')
+  }
+  else {
+    root.classList.remove('active')
+    page_center.classList.remove('active')
+  }
+})
+
+function search() {
+  if (!keywordQueue.length) return
+  keyword = keywordQueue.pop()
+  keywordQueue = []
+  
+  httpRequest = new XMLHttpRequest()
+  httpRequest.onreadystatechange = () => {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+      if (httpRequest.status === 200) {
+        console.log('got response')
+      }
+    }
+  }
+  httpRequest.open('GET', `http://localhost:3000/search?key=${keyword}&cid=${window.location.pathname.slice(7)}`, true)
+  httpRequest.send()
+  console.log(keyword)
+}
+setInterval(search, 1000)
